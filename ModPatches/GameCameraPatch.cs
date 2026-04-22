2e19768c080773711b528d1bb7f7a630f720dd32
Denyscrasav4ik <horanden238@gmail.com>
2026-04-22 22:12:35 +0300

    Remake the subtitles for the new phiisophy. I swear if mystman12 reverts this change.

--- ModPatches/GameCameraPatch.cs
+++ ModPatches/GameCameraPatch.cs
@@ -3,39 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace BBTimes.ModPatches
 {
-	[HarmonyPatch(typeof(GameCamera), "Awake")]
-	internal class GameCameraPatch
-	{
-		private static void Prefix(GameCamera __instance)
-		{
-			var visual = Object.Instantiate(playerVisual, __instance.transform);
-			visual.transform.localPosition = Vector3.zero;
-			visual.Initialize(__instance);
-		}
+    [HarmonyPatch(typeof(GameCamera), "Awake")]
+    internal class GameCameraPatch
+    {
+        private static void Prefix(GameCamera __instance)
+        {
+            var visual = Object.Instantiate(playerVisual, __instance.transform);
+            visual.transform.localPosition = Vector3.zero;
+            visual.Initialize(__instance);
+        }
 
-		static internal PlayerVisual playerVisual;
-	}
-	public class PlayerVisual : MonoBehaviour 
-	{
-		[SerializeField]
-		internal Sprite[] emotions;
+        static internal PlayerVisual playerVisual;
+    }
+    public class PlayerVisual : MonoBehaviour
+    {
+        [SerializeField]
+        internal Sprite[] emotions;
 
-		[SerializeField]
-		internal SpriteRenderer renderer;
-		int id = -1;
+        [SerializeField]
+        internal SpriteRenderer renderer;
+        int id = -1;
 
-		readonly static Dictionary<int, PlayerVisual> visuals = [];
-		public static PlayerVisual GetPlayerVisual(int id) => visuals[id];
-		public void SetEmotion(int id) =>
-			renderer.sprite = emotions[id];
+        readonly static Dictionary<int, PlayerVisual> visuals = [];
+        public static PlayerVisual GetPlayerVisual(int id) => visuals[id];
+        public void SetEmotion(int id) =>
+            renderer.sprite = emotions[id];
 
-		public void Initialize(GameCamera cam) 
-		{
-			visuals.Add(cam.camNum, this);
-			id = cam.camNum;
-		}
+        public void Initialize(GameCamera cam)
+        {
+            id = cam.camNum;
+            visuals[id] = this;
+        }
 
-		void OnDestroy() =>
-			visuals.Remove(id);
-	}
+        void OnDestroy() =>
+            visuals.Remove(id);
+    }
 }
